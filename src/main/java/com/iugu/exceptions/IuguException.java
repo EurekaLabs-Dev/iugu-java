05753eecb29f3f620b86b7ca89a80b18472dbd25
ktabriga <jhonatancatabriga@gmail.com>
2018-03-08 19:05:52 -0300

    Adiciona satus na exceção

--- src/main/java/com/iugu/exceptions/IuguException.java
+++ src/main/java/com/iugu/exceptions/IuguException.java
@@ -3,13 +3,19 @@ package com.iugu.exceptions;
 public class IuguException extends Exception {
 	private static final long serialVersionUID = 8701207870125630038L;
 	private final String responseMessage;
+	private final int status;
 
 	public IuguException(String Message, int ResponseCode, String ResponseText) {
 		super(Message + " - StatusCode: [" + ResponseCode + "] / ResponseText: [" + ResponseText + "]");
 		responseMessage = ResponseText;
+		status = ResponseCode;
 	}
 
 	public String getResponseMessage() {
 		return responseMessage;
+	}
+
+	public int getStatus() {
+		return status;
 	}
 }
